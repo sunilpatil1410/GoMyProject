45fee38dffc3538b433bf94aa8f65005ff64e57e
vshinde <vshinde@sscinc.com>
2020-03-22 18:30:05 +0530

    Added changes done in office. 2020-03-22

--- GoMyProject/target/classes/static/js/autocomplete.js
+++ GoMyProject/target/classes/static/js/autocomplete.js
@@ -1,15 +1,59 @@
+
+ //to load nav bar
+$(function(){
+	  $("#nav-placeholder").load("Nav");
+});
+
 jQuery(document).ready(function($) {
-	jQuery('#client').change(function(event) {
-			alert(" inside client ");
-			console.log("inside client");
-		});
+
+	// to get the client list in drop down.
+	jQuery("#client").autocomplete({
+		source : "\clientList",
+		minLength : 0,
+		scroll : true,
+		select : function(event, ui) {
+			this.value = ui.item.label;
+			$("#client").val(ui.item.label);
+			//console.log(this.value);
+			//console.log(ui.item);
+			$("#clientId").val(ui.item.value);
+			alert("INSID");
+			jQuery.getJSON("getFundsFromClient", {
+				p1 : ui.item.label
+			}, function(responseData) {
+				console.log(responseData);
+				//var clientFunds = jQuery('#clientFunds');
+				jQuery('#funds').empty();
+				var clientFunds = jQuery('#funds');
+				jQuery.each(responseData, function(key, value) {
+					console.log(value.label + " >> "+value.value);										
+					jQuery('<option id =' +value.value+'>').val(value.value)
+            	    .text(value.label).appendTo(clientFunds);			
+				});
+
+			});
+			return false;
+		}
+	});
+	
+	//label	value
+			
+	$("#funds").select2({
+		  maximumSelectionLength: 2
+	});
+
+	$("#funds").select2({
+		  placeholder: 'Select Funds',
+		  allowClear: true
+	});
 	
 	
+	// its check if clien field is blank then blank clientId as well
 	jQuery('#client').change(function(event) {		
 		//let clientLength = $('#client').val().length;
 		//console.log(clientLength);
 		if( $('#client').val() == "" ){
 			$("#clientId").val("");
 		}	
-	});
+	}); 
 });
