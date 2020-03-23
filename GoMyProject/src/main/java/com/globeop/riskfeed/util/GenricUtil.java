84a5fc73ca4c25f4982a26ff39bb0714da04adc2
VIRENDRA <VIRENDRA@DESKTOP-UITG6CG>
2020-03-23 14:16:52 +0530

    Commented getClientFundList function and added local fundList getClientFundList function. 2020-03-23

--- GoMyProject/src/main/java/com/globeop/riskfeed/util/GenricUtil.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/util/GenricUtil.java
@@ -7,69 +7,78 @@ import java.util.List;
 import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 
-import com.globeop.gocheck.api.ClientRetriever;
-import com.globeop.gocheck.api.ClientRetrieverFactory;
-import com.globeop.gocheck.api.FundRetriever;
-import com.globeop.gocheck.api.FundRetrieverFactory;
-import com.globeop.gocheck.api.GoCheckApiConfig;
-import com.globeop.gocheck.api.GoCheckApiConfigParser;
-import com.globeop.gocheck.core.Client;
-import com.globeop.gocheck.core.Fund;
-import com.globeop.gocheck.core.FundStatus;
+
 import com.globeop.riskfeed.dto.LabelValueDto;
 
 
 public class GenricUtil {
 
-    public static List<LabelValueDto> getClientFundList(String clientShortname){
-    	List<LabelValueDto> list=new ArrayList<LabelValueDto>();
-        try{
-            Class clazz = Class.forName("org.apache.xerces.parsers.SAXParser");
-            XMLReader reader = (XMLReader) clazz.newInstance();
-            GoCheckApiConfigParser goCheckApiConfigParser = new GoCheckApiConfigParser(reader);
-            reader.setContentHandler(goCheckApiConfigParser);
-            reader.parse(new InputSource(Resources.getResourceAsStream("templates/gocheck-api-config.xml")));
-            GoCheckApiConfig config = goCheckApiConfigParser.getGoCheckClientConfig();
-
-            FundRetriever fundRetriever = FundRetrieverFactory.newInstance(config);
-            ClientRetriever clientRetriever = ClientRetrieverFactory.newInstance(config);
-            Client client=clientRetriever.getClient(clientShortname);
-            System.out.println(client.getDescription()+">>"+client.getClientId());
-
-            @SuppressWarnings("unchecked")
-			List<Fund> fundList=fundRetriever.getFundListForClient(client.getClientId());
-            
-            for(Fund f:fundList){
-                FundStatus fs;
-                String status="";
-                try{
-                    fs = f.getStatus();
-                    status=fs.getName();
-                }catch (Exception e){
-
-                }
-                
-                if("Active".equals(status) && f.isTradingEntity()) {
-                	LabelValueDto labelValueDto = new LabelValueDto();
-                	labelValueDto.setLabel(f.getShortName());
-                    labelValueDto.setValue(f.getShortName());
-                    list.add(labelValueDto);                    
-                }
-                //System.out.println(f.getShortName() + ","+status+","+f.getLaunchDate() +","+f.isTradingEntity());
-            }
-            //System.out.println(list);
-            
-            //list.forEach(System.out::println);
-            //list.stream().forEach(System.out::println);
-
-        }catch (Exception e){
-            e.printStackTrace();
-
-        }
-        return list;
-    }
+	/*
+	 * public static List<LabelValueDto> getClientFundList(String clientShortname){
+	 * List<LabelValueDto> list=new ArrayList<LabelValueDto>(); try{ Class clazz =
+	 * Class.forName("org.apache.xerces.parsers.SAXParser"); XMLReader reader =
+	 * (XMLReader) clazz.newInstance(); GoCheckApiConfigParser
+	 * goCheckApiConfigParser = new GoCheckApiConfigParser(reader);
+	 * reader.setContentHandler(goCheckApiConfigParser); reader.parse(new
+	 * InputSource(Resources.getResourceAsStream("templates/gocheck-api-config.xml")
+	 * )); GoCheckApiConfig config =
+	 * goCheckApiConfigParser.getGoCheckClientConfig();
+	 * 
+	 * FundRetriever fundRetriever = FundRetrieverFactory.newInstance(config);
+	 * ClientRetriever clientRetriever = ClientRetrieverFactory.newInstance(config);
+	 * Client client=clientRetriever.getClient(clientShortname);
+	 * System.out.println(client.getDescription()+">>"+client.getClientId());
+	 * 
+	 * @SuppressWarnings("unchecked") List<Fund>
+	 * fundList=fundRetriever.getFundListForClient(client.getClientId());
+	 * 
+	 * for(Fund f:fundList){ FundStatus fs; String status=""; try{ fs =
+	 * f.getStatus(); status=fs.getName(); }catch (Exception e){
+	 * 
+	 * }
+	 * 
+	 * if("Active".equals(status) && f.isTradingEntity()) { LabelValueDto
+	 * labelValueDto = new LabelValueDto();
+	 * labelValueDto.setLabel(f.getShortName());
+	 * labelValueDto.setValue(f.getShortName()); list.add(labelValueDto); }
+	 * //System.out.println(f.getShortName() + ","+status+","+f.getLaunchDate()
+	 * +","+f.isTradingEntity()); } //System.out.println(list);
+	 * 
+	 * //list.forEach(System.out::println);
+	 * //list.stream().forEach(System.out::println);
+	 * 
+	 * }catch (Exception e){ e.printStackTrace();
+	 * 
+	 * } return list; }
+	 */
   
 
+	
+	public static List<LabelValueDto> getClientFundList(String clientShortname){
+		 List<LabelValueDto> list=new ArrayList<LabelValueDto>();
+		 LabelValueDto l1 = new LabelValueDto();
+		 l1.setLabel("FUND1");
+		 l1.setValue("FUND1");
+		 
+		 LabelValueDto l2 = new LabelValueDto();
+		 
+		 l2.setLabel("FUND2");
+		 l2.setValue("FUND2");
+		 
+		 LabelValueDto l3 = new LabelValueDto();
+		 l3.setLabel("FUND3");
+		 l3.setValue("FUND3");
+		 
+		 LabelValueDto l4 = new LabelValueDto();
+		 l4.setLabel("FUND4");
+		 l4.setValue("FUND4");
+		 
+		 list.add(l1);
+		 list.add(l2);
+		 list.add(l3);
+		 list.add(l4);
+		 return list;
+	}
     public static void main(String[] args) throws Exception {        
     	getClientFundList("BFAM");
     }
