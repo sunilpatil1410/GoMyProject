ab14597faf55115fa558b8a0f39dfe68127f725a
VIRENDRA <VIRENDRA@DESKTOP-UITG6CG>
2020-03-27 19:21:38 +0530

    Added for testing.

---
+++ GoMyProject/src/main/java/com/globeop/riskfeed/dto/OnBoardFunds.java
@@ -0,0 +1,16 @@
+package com.globeop.riskfeed.dto;
+
+public class OnBoardFunds {
+	
+	private String fundName;
+
+	public String getFundName() {
+		return fundName;
+	}
+
+	public void setFundName(String fundName) {
+		this.fundName = fundName;
+	}
+	
+	
+}
